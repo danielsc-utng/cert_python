d1eeaeff7008b2b9726b71159d34565704dd8b7d
danielsc-utng <luisdanielutng@gmail.com>
2022-06-07 17:22:35 -0500

    Realización del laboratorio 2.1.1.18

---
+++ basico/modulo2/lab21118.py
@@ -0,0 +1,5 @@
+# Laboratorio 2.1.1.18
+#Luis Daniel Sánchez Cabrera
+
+print("Fundamentos","Programación","en", sep="***", end="...")
+print("Python")
--- basico/modulo2/lab2116.py
+++ basico/modulo2/lab2116.py
@@ -1,4 +1,5 @@
 # Laboratorio 2.1.1.6
+#Luis Daniel Sánchez Cabrera
 print("HOLA MUNDO")
 print()
 print("Luis Daniel Sánchez Cabrera")
