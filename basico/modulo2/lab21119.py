81a382db2c4dec269d752bef73981b80e65850e7
danielsc-utng <luisdanielutng@gmail.com>
2022-06-07 17:48:21 -0500

    Elaboración de laboratorio 2.1.1.19

---
+++ basico/modulo2/lab21119.py
@@ -0,0 +1,27 @@
+# Laboratorio 2.1.1.19
+#Luis Daniel Sánchez Cabrera
+
+print("Disminuyendo el uso de funciones print")
+print("     *","\n","   * *","\n","  *   *","\n"," *     *","\n","***   ***","\n","  *   *","\n","  *   *","\n","  *****")
+
+print("Aumentando el tamaño")
+
+print("    *","\n")
+print("   * *","\n")
+print("  *   *","\n")
+print(" *     *","\n")
+print("***   ***","\n")
+print("  *   *","\n")
+print("  *   *","\n")
+print("  *****","\n")
+
+print("Doble flecha")
+
+print("    *      "*2)
+print("   * *     "*2)
+print("  *   *    "*2)
+print(" *     *   "*2)
+print("***   ***  "*2)
+print("  *   *    "*2)
+print("  *   *    "*2)
+print("  *****    "*2)
