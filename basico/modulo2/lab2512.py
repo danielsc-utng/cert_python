25f966d3f19e39a632fd24dce43a037074603afc
danielsc-utng <luisdanielutng@gmail.com>
2022-06-10 20:41:40 -0500

    elaboración de laboratorio 2.5.1.2

---
+++ basico/modulo2/lab2512.py
@@ -0,0 +1,16 @@
+# Laboratorio 2.5.1.2
+#Luis Daniel Sánchez Cabrera
+
+# este programa calcula los segundos en cierto número de horas determinadas 
+# este programa fue escrito hace dos días
+
+horas= 2 # número de horas
+seconds= 3600 # número de segundos en una hora
+
+print("Horas: ", horas) #imprime el numero de horas
+print("Segundos en Horas: ", horas * seconds) # se imprime el numero de segundos en determinado numero de horas
+
+'''
+aquí también se debe de imprimir un "Adiós"
+este el es fin del programa que calcula el numero de segundos en 2 horas
+'''
