94336cde1d0844075cb701f8d6efc63acb274f04
Luis Daniel Sánchez Cabrera <luisdanielutng@gmail.com>
2022-06-12 23:16:55 -0500

    Elaboración del laboratorio 2.6.1.10

---
+++ basico/modulo2/lab26110.py
@@ -0,0 +1,12 @@
+# Laboratorio 2.6.1.10
+#Luis Daniel Sánchez Cabrera
+
+from re import X
+
+
+x=float(input("Ingresa el valor para x: "))
+
+#Escribe tu código aquí,
+
+y=(1/(x+1/(x+1/(x+(1/x)))))
+print("y =", y)
