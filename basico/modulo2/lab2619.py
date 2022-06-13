be322ad8165bc35c452c03468fdfb153ac879f7c
Luis Daniel Sánchez Cabrera <luisdanielutng@gmail.com>
2022-06-12 23:03:50 -0500

    Elaboración laboratorio 2.6.1.9

---
+++ basico/modulo2/lab2619.py
@@ -0,0 +1,20 @@
+# Laboratorio 2.6.1.9
+#Luis Daniel Sánchez Cabrera
+
+# ingresa un valor flotante para la variable a aquí
+variableA=float(input("Ingrese un valor flotante para la variable a "))
+# ingresa un valor flotante para la variable b aquí
+variableB=float(input("Ingrese un valor flotante para la variable b "))
+# muestra el resultado de la suma aquí
+suma=variableA+variableB
+print("La suma es "+ str(suma))
+# muestra el resultado de la resta aquí
+resta=variableA-variableB
+print("La resta es "+ str(resta))
+# muestra el resultado de la multiplicación aquí
+multi=variableA*variableB
+print("La multiplicación es "+ str(multi))
+# muestra el resultado de la división aquí
+div=variableA/variableB
+print("La división es "+ str(div))
+print("\n¡Eso es todo, amigos!")
