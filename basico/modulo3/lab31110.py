fed855e48ac87d4e8c27e301a43362b06226ff2f
danielsc-utng <luisdanielutng@gmail.com>
2022-06-14 16:37:33 -0500

    Elaborción del laboratorio 3.1.1.10

---
+++ basico/modulo3/lab31110.py
@@ -0,0 +1,11 @@
+# Laboratorio 3.1.1.10
+#Luis Daniel Sánchez Cabrera
+
+word= input("Ingresa el nombre de la planta: ")
+
+if word == "ESPATIFILIO":
+    print("Si, ¡El ESPATIFILIO es la mejor planta de todos los tiempos!")
+elif word == "espatifilio":
+    print ("No, ¡quiero un gran ESPATIFILIO!")
+else:
+    print ("!ESPATIFILIO!, ¡No pelargonio!")
