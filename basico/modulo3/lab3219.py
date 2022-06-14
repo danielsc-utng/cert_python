5d96028398d1ea5d48e8987c0e74aad853615ffa
danielsc-utng <luisdanielutng@gmail.com>
2022-06-14 17:46:54 -0500

    Elaborción del laboratorio 3.2.1.9

---
+++ basico/modulo3/lab3219.py
@@ -0,0 +1,11 @@
+# Laboratorio 3.2.1.9
+#Luis Daniel Sánchez Cabrera
+
+
+res="chupacabra"
+
+while True:
+    word = input("Ingresa la palabra: ")
+    if word == res:
+        print("¡Has dejado el bucle con éxito!")
+        break
