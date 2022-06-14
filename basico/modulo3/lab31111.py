ea7eda7eaa5b32f5d82732c1f106e2c23f846ca1
danielsc-utng <luisdanielutng@gmail.com>
2022-06-14 17:01:37 -0500

    Elaborción del laboratorio 3.1.1.11

---
+++ basico/modulo3/lab31111.py
@@ -0,0 +1,13 @@
+# Laboratorio 3.1.1.10
+#Luis Daniel Sánchez Cabrera
+
+income=float(input("Introduce el ingreso anual:"))
+
+
+if income < 85_528:
+    ing=income * 0.18 
+    tax=ing - 556.02
+else:
+    tax=14839.02+(income-85528)*0.32
+tax=round(tax, 0)
+print("El impuesto es:", tax, "pesos")
