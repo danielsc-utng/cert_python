c66d2366d306a124f51a675133f97feb1df225a3
Luis Daniel Sánchez Cabrera <luisdanielutng@gmail.com>
2022-06-17 12:26:17 -0500

    Elaboración del laboratorio 3.2.1.10

---
+++ basico/modulo3/lab32110.py
@@ -0,0 +1,23 @@
+# Laboratorio 3.2.1.10
+#Luis Daniel Sánchez Cabrera
+
+# Indicar al usuario que ingrese una palabra
+# y asignarlo a la variable user_word.
+user_word = input("Ingrese una palabra ")
+user_word = user_word.upper()
+
+# Completa el cuerpo del bucle for.
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
+    
